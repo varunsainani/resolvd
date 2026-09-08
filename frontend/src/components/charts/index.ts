d8c1d61756df0610b28682af87b1e1c86e741f1d
varunsainani <varunsainani0990@gmail.com>
2026-09-08 20:45:59 +0500

    feat(charts): barrel

---
+++ frontend/src/components/charts/index.ts
@@ -0,0 +1,5 @@
+// Barrel for the chart primitives.
+export { BarList, type BarItem } from "./bar-list";
+export { BarListRow } from "./bar-list-row";
+export { TrendChart } from "./trend-chart";
+export { ChartEmpty } from "./chart-empty";
