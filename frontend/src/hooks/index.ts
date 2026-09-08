854572b0e3eae5c02b61e6c2e18c108a9cbfc692
varunsainani <varunsainani0990@gmail.com>
2026-09-08 20:10:21 +0500

    feat(hooks): export useTicketDetail

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -4,3 +4,4 @@ export { useMutation, type MutationState } from "./use-mutation";
 export { useDebounce } from "./use-debounce";
 export { useDisclosure } from "./use-disclosure";
 export { useInboxFilters, type InboxFiltersController } from "./use-inbox-filters";
+export { useTicketDetail, type TicketDetailState } from "./use-ticket-detail";
