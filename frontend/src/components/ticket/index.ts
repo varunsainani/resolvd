b6f516bd3a79c1cc6a0ff27d876ec1df8c0d4a32
varunsainani <varunsainani0990@gmail.com>
2026-09-08 20:15:43 +0500

    feat(ticket): barrel for ticket detail components

---
+++ frontend/src/components/ticket/index.ts
@@ -0,0 +1,10 @@
+// Barrel for the ticket detail components.
+export { TicketDetail } from "./ticket-detail";
+export { TicketActionsProvider, useTicketActions, type TicketActions } from "./ticket-context";
+export { TicketHeader } from "./ticket-header";
+export { TicketBackLink } from "./ticket-back-link";
+export { MessageThread } from "./message-thread";
+export { ReplyComposer } from "./reply-composer";
+export { SuggestButton } from "./suggest-button";
+export { TicketSidebar } from "./ticket-sidebar";
+export { TicketNotFound } from "./ticket-not-found";
