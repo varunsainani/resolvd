de1f127106b3bc4f4c02321c4400194194328c29
varunsainani <varunsainani0990@gmail.com>
2026-09-10 00:10:53 +0500

    feat(canned): barrel

---
+++ frontend/src/components/canned/index.ts
@@ -0,0 +1,5 @@
+// Barrel for the canned responses components.
+export { CannedView } from "./canned-view";
+export { CannedList } from "./canned-list";
+export { CannedCard } from "./canned-card";
+export { CannedFormModal } from "./canned-form-modal";
