aa3d2f6118ef78b14c6386aaafadff4cd3fe74a7
varunsainani <varunsainani0990@gmail.com>
2026-09-10 00:01:38 +0500

    feat(settings): barrel

---
+++ frontend/src/components/settings/index.ts
@@ -0,0 +1,5 @@
+// Barrel for the settings components.
+export { SettingsView } from "./settings-view";
+export { ProfileForm } from "./profile-form";
+export { PreferencesForm } from "./preferences-form";
+export { ThemeSelector } from "./theme-selector";
