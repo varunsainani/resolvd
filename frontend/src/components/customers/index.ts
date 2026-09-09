a7204c3b44f8a429302f199c9eff6b30ab127aba
varunsainani <varunsainani0990@gmail.com>
2026-09-10 00:05:08 +0500

    feat(customers): barrel

---
+++ frontend/src/components/customers/index.ts
@@ -0,0 +1,5 @@
+// Barrel for the customers components.
+export { CustomersView } from "./customers-view";
+export { CustomerTable } from "./customer-table";
+export { CustomerDetail } from "./customer-detail";
+export { EditCustomerModal } from "./edit-customer-modal";
