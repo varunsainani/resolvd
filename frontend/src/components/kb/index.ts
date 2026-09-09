3f5bead6a723b81abe8ca2166bc75056f173aaa0
varunsainani <varunsainani0990@gmail.com>
2026-09-10 00:08:43 +0500

    feat(kb): barrel

---
+++ frontend/src/components/kb/index.ts
@@ -0,0 +1,5 @@
+// Barrel for the knowledge base components.
+export { KbView } from "./kb-view";
+export { KbList } from "./kb-list";
+export { KbCard } from "./kb-card";
+export { KbFormModal } from "./kb-form-modal";
